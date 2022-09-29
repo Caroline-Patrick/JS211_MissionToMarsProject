313fcbbe2e735fd79a63992034f4a383727feeb5
BoltenCoder <moltencreeper25@gmail.com>
2022-09-28 20:59:54 -0500

    Done in class.

--- main.js
+++ main.js
@@ -9,12 +9,34 @@ const jobTypes = {
   programmer: 'Any Ship!'
 };
 
-// Your code will go here
+class CrewMember {
+  constructor(name, job, specialSkill, ship) {
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = ship;
+  }
+  enterShip(shipName) {
+    this.ship = shipName
+    shipName.crew.push(this)
+  }
+}
 
-
-
-
-
+class Ship {
+  constructor(name, type, ability, crew){
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
+  }
+  missionStatement(){
+    if (this.crew.length == 0) {
+      return "Can't perform a mission yet."
+    } else {
+      return this.ability
+    }
+  }
+} 
 
 
 // Begin by reading the tests and building a function that will full each one.
